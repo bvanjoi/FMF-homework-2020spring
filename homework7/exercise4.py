83dc8367a6b722141a473ac23d32d77674d1934c
bvanjoi <610376039@qq.com>
2020-07-12 14:21:33 +0800

    fix: 费马大定理验证部分的更新

--- homework7/exercise4.py
+++ homework7/exercise4.py
@@ -1,4 +1,5 @@
 from z3 import *
+import time 
 
 class Todo(Exception):
     pass
@@ -33,12 +34,35 @@ def fermat(x, y, z, n):
     cons.append(z!=0)
     def mul(k,n):
         temp = 1
-        for i in range(n):
+        for _ in range(n):
             temp *= k
         return temp
         
     cons.append(mul(x,n)+mul(y,n) == mul(z,n))
     return cons
+
+
+a,b,c = Ints('a b c')
+def fermat_int(a, b, c, n):
+    '''  测试整数，用来回答ppt上的问题
+    '''
+    cons = []
+    max_number = 511
+    cons.append(a<max_number)  # I can't understand why the magic number is that.
+    cons.append(b<max_number)
+    cons.append(c<max_number)
+    cons.append(a!=0)
+    cons.append(b!=0)
+    cons.append(c!=0)
+    def mul(k,n):
+        temp = 1
+        for _ in range(n):
+            temp *= k
+        return temp
+        
+    cons.append(mul(a,n)+mul(b,n) == mul(c,n))
+    return cons
+
 
 if __name__ == '__main__':
     # some unit tests
@@ -50,10 +74,23 @@ if __name__ == '__main__':
     else:
         print("We are more confident that Fermat's Last Theorem does held, although we don't have a rigorous proof yet")
     solver = Solver()
-    solver.add(fermat(x, y, z, 4))
+    solver.add(fermat(x, y, z, 3))
     res = solver.check()
+    start_time = time.time()
     if res == sat:
         print("found a counter example, Fermat's Last Theorem does NOT held: ", solver.model())
     else:
         print("We are more confident that Fermat's Last Theorem does held, although we don't have a rigorous proof yet")
+    end_time = time.time()
+    print('bit vec prove fermat:', end_time - start_time)
 
+    solver = Solver()
+    solver.add(fermat_int(a, b, c, 3))
+    res = solver.check()
+    start_time = time.time()
+    if res == sat:
+        print("found a counter example, Fermat's Last Theorem does NOT held: ", solver.model())
+    else:
+        print("We are more confident that Fermat's Last Theorem does held, although we don't have a rigorous proof yet")
+    end_time = time.time()
+    print('ints prove fermat:', end_time - start_time)
